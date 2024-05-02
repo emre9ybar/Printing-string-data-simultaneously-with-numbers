adacc6d5c9ff49a2f539274e8f5c8f506a9063d0
emre aybar <aybaremre5@gmail.com>
2024-05-02 17:12:02 +0300

    java

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+        public class Main {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("RAKAM VE SAYIYI GİRİNİZ:");
+                while (scanner.hasNext()) {
+                    String a = scanner.next();
+                    String[] b = a.split(",");
+
+                    int x = scanner.nextInt();
+                    System.out.printf("isim ve rakam :"+"%-5s%03d%n", a, x);
+                }
+                System.out.println();
+            }
+        }
